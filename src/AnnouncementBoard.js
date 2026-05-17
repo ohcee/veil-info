23d5c27008d3d5b21678ef15fa8ea5865e1f8f50
ohcee <46406370+ohcee@users.noreply.github.com>
2026-05-17 14:52:18 -0700

    Add link to Zelcore Wallet in AnnouncementBoard

--- src/AnnouncementBoard.js
+++ src/AnnouncementBoard.js
@@ -9,6 +9,7 @@ const AnnouncementBoard = () => {
         <li><h2><a href="https://veil-project.com/blog/" target="_blank" rel="noopener noreferrer">Blog</a></h2></li>
         <li><h2><a href="https://github.com/Veil-Project/veil/releases" target="_blank" rel="noopener noreferrer">Core Wallet</a></h2></li>
         <li><h2><a href="https://veilproject.org" target="_blank" rel="noopener noreferrer">Light Wallet</a></h2></li>
+        <li><h2><a href="https://zelcore.io/https://zelcore.io/" target="_blank" rel="noopener noreferrer">Zelcore Wallet</a></h2></li>
         <li><h2><a href="https://explorer.veil-project.com" target="_blank" rel="noopener noreferrer">Explorer</a></h2></li>
         <li><h2><a href="https://veil.tools/" target="_blank" rel="noopener noreferrer">Veil Tools</a></h2></li>
         <li><h2><a href="https://nonkyc.io/market/VEIL_USDT" target="_blank" rel="noopener noreferrer">NonKYC Exchange</a></h2></li>
